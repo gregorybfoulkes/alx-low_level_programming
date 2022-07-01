f3064cac189a98371356b0f84edcbbb6819a3f82
gregorybfoulkes <gregorybfoulkes@gmail.com>
2022-07-01 05:12:41 -0700

    added 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
-		if (i != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
