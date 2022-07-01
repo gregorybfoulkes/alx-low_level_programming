6692806d7c500896323bccf5f4acf45c95becc1e
gregorybfoulkes <gregorybfoulkes@gmail.com>
2022-07-01 03:21:38 -0700

    added 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -25,5 +25,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	return(0)
+	return (0);
 }
