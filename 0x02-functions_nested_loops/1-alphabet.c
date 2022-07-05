786a08a9ba1efe4296424d3610a12ce289bf448c
gregorybfoulkes <gregorybfoulkes@gmail.com>
2022-07-05 07:44:51 -0700

    added 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,10 @@
 /**
  * print_alphabet - prints alphabet in lowercase, followed by a new line
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar (letter);
