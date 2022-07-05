4e481bc99d9ceccd1759bb1c5daefeb1f4cc1f27
gregorybfoulkes <gregorybfoulkes@gmail.com>
2022-07-05 10:24:38 -0700

    added 100- times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,25 +21,22 @@ void print_times_table(int n)
 				_putchar(' ');
 				result = digit * mult;
 				if (result <= 99)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
+
 				if (result <= 9)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
+
 				if (result >= 100)
 				{
-					_putchar((result / 100) + '0');
-					_putchar((result / 10) % 10 + '0');
+				_putchar((result / 100) + '0');
+				_putchar((result / 10) % 10 + '0');
 				}
 				else if (result <= 99 && result >= 10)
 				{
-					_putchar((result / 10) + '0');
+				_putchar((result / 10) + '0');
 				}
-					_putchar((result % 10) + '0');
+				_putchar((result % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-}
