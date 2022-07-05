52fb23c0c60c878a55b61d1a0b1e8e285cabd609
gregorybfoulkes <gregorybfoulkes@gmail.com>
2022-07-05 08:31:40 -0700

    added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints out sign of a number
+ * @n: character to be verified
+ * Return: 0 if n == 0, 1 if n > 0, -1 otherwise
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+	return (1);
+	}
+	else
+	{
+		_putchar('-');
+	return (-1);
+	}
+}
